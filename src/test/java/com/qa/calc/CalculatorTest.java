5fab283f54c490e39449de391d18252898679929
klausendias <klaus_dias1@hotmail.co.uk>
2019-06-24 15:58:38 +0100

    Divide

--- src/test/java/com/qa/calc/CalculatorTest.java
+++ src/test/java/com/qa/calc/CalculatorTest.java
@@ -16,8 +16,23 @@ public class CalculatorTest {
 	@Test
 	public void SubtractTwoNumbersTest() {
 		Calculator calculator = new Calculator ();
-		int result = calculator.add(2,2);
-		Assert.assertEquals(0, result);
+		int result = calculator.subtract(3,2);
+		Assert.assertEquals(1, result);
 	}
+	
+	@Test
+	public void MuliplyTwoNumbersTest() {
+		Calculator calculator = new Calculator ();
+		int result = calculator.multiply(3,2);
+		Assert.assertEquals(6, result);
+	}
+
+	@Test
+	public void DivideTwoNumbersTest() {
+		Calculator calculator = new Calculator ();
+		int result = calculator.divide(4,2);
+		Assert.assertEquals(2, result);
+	}
+
 
 }
